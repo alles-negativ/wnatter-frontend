6846ca8f93bd9865eccb108b08be2e40421a845b
Carlo <61711997+GenepoolWoW@users.noreply.github.com>
2022-07-07 15:29:10 +0200

    [Add]: Articles number

--- components/Articles/Articles.vue
+++ components/Articles/Articles.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="container" id="angebot"> 
         <div class="container__content" v-for="article in articles" :key="article.id">
-            <h1 class="container__content--title" v-html="article.contenttitle"></h1>
+            <div class="container__content--head">
+                <p class="container__content--number">{{article.number}}</p>
+                <h1 class="container__content--title" v-html="article.contenttitle"></h1>
+            </div>
             <div class="container__content--text" v-html="article.contenttext"></div>
         </div>
     </div>
@@ -20,6 +23,7 @@ export default {
         const { json: data } = await this.$kirby.find({
             "query": "page('home').children",
             "select": {
+                "number": true,
                 "contenttitle": true,
                 "contenttext": true,
             }
